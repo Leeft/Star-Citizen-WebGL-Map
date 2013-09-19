88badba95a46a57185b90e3dd861d4bed6f0ced4
Lianna Eeftinck <liannaee@gmail.com>
2013-09-19 11:51:40 +0100

    Moved and renamed files to be more obvious and match the code better, also removed dos carriage returns

---
+++ js/data/map.js
@@ -0,0 +1,90 @@
+var sc_map = {
+   "UUE": {
+      "color": 8421616,
+      "systems": {
+         "Sol": { "scale": 1, "coords": [ 0, 45, 0 ] },
+         "Vega": { "scale": 1, "coords": [ -106, 36, -88 ] },
+         "Croshaw": { "scale": 1, "coords": [ 12, 69, 104 ] },
+         "Ferron": { "scale": 1, "coords": [ 78, 54, 227 ] },
+         "Centauri": { "scale": 1, "coords": [ -192, 80, 258 ] },
+         "Garron": { "scale": 1, "coords": [ -220, 106, 4 ] },
+         "Stanton": { "scale": 1, "coords": [ 100, 96, -194 ] },
+         "Davien": { "scale": 1, "coords": [ 159, 47, 160 ] },
+         "Tohil": { "scale": 1, "coords": [ 303, 85, -425 ] },
+         "Bremen": { "scale": 1, "coords": [ -268, 64, -497 ] },
+         "Chronos": { "scale": 1, "coords": [ -60, 32, -364 ] }
+      },
+      "known_routes": {
+         "Sol": [ "Vega", "Croshaw", "Stanton" ],
+         "Vega": [ "Garron", "Kellog", "Virgil" ],
+         "Stanton": [ "Chronos", "Pelles", "Kallis" ],
+         "Croshaw": [ "Nul", "Davien", "Centauri", "Rhetor" ],
+         "Davien": [ "Ferron", "Cathcart" ],
+         "Ferron": [ "Idris", "Davien" ],
+         "Chronos": [ "Kellog", "Bremen", "Branaugh" ],
+         "Tohil": [ "Oso", "Pallas" ],
+         "Bremen": [ "Branaugh", "Kellog" ],
+         "Garron": [ "Nul" ],
+         "Centauri": [ "Nul", "Elysium" ]
+      }
+   },
+   "Vanduul": {
+      "color": 15761536,
+      "systems": {
+         "Caliban": { "scale": 1, "coords": [ -293, 40, 125 ] },
+         "Vanguard": { "scale": 1, "coords": [ -462, 102, 208 ] },
+         "Orion": { "scale": 1, "coords": [ -432, 33, 72 ] },
+         "Tiber": { "scale": 1, "coords": [ -362, 79, -48 ] },
+         "Virgil": { "scale": 1, "coords": [ -288, 71, -108 ] },
+         "Vendetta": { "scale": 1, "coords": [ -494, 78, -288 ] }
+      },
+      "known_routes": {
+         "Vendetta": [ "Kellog" ],
+         "Tiber": [ "Virgil", "Garron", "Orion" ],
+         "Orion": [ "Caliban", "Vanguard" ],
+         "Caliban": [ "Vanguard", "Nul" ],
+         "Vulture": [ "Elysium", "Leir" ]
+      }
+   },
+   "Xi'An": {
+      "color": 8454016,
+      "systems": {
+         "Pallas": { "scale": 1, "coords": [ 200, 75, -565 ] }
+      },
+      "known_routes": {
+         "Pallas": [ "Gliese", "Tohil", "Hadur", "Oya" ]
+      }
+   },
+   "Banu": {
+      "color": 15790208,
+      "systems": {
+         "Gliese": { "scale": 1, "coords": [ 132, 56, -617 ] }
+      },
+      "known_routes": {
+         "Gliese": [ "Branaugh", "Pallas" ]
+      }
+   },
+   "Other": {
+      "color": 15790320,
+      "systems": {
+         "Kellog": { "scale": 1, "coords": [ -325, 52, -327 ] },
+         "Kallis": { "scale": 1, "coords": [ 204, 95, -54 ] },
+         "Oso": { "scale": 1, "coords": [ 270, 80, -167 ] }
+      },
+      "known_routes": {
+         "Kallis": [ "Oso" ],
+         "Oso": [ "Hades", "Castra" ]
+      }
+   },
+   "None": {
+      "color": 12632256,
+      "systems": {
+         "Nul": { "scale": 1, "coords": [ -158, 72, 132 ] },
+         "Pelles": { "scale": 1, "coords": [ 217, 136, -298 ] },
+         "Branaugh": { "scale": 1, "coords": [ -130, 78, -494 ] }
+      },
+      "known_routes": {
+         "Pelles": [ "Castra" ]
+      }
+   }
+};
